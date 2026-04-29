dc93e25b83799ec0becb1299e5e85a517cace2b0
Dmytro <dima.shutko5@gmail.com>
2026-04-30 00:55:14 +0200

    solu2

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,5 @@
 
 kind create cluster --config cluster.yml
 kubectl taint nodes -l app=mysql app=mysql:NoSchedule
-helm install release helm-chart/todoapp/
+helm dependency build .infrastructure/todoapp/
+helm install release .infrastructure/todoapp/
